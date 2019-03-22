ea62f5229a80ba413aee7d71a29d67cce5fc311f
zhao_coding <837406479@qq.com>
2019-03-22 17:22:57 +0800

    完善上拉下拉控件

--- utools/src/main/java/com/zking/widget/refreshview/swipeMenu/OnSwipeMenuListener.java
+++ utools/src/main/java/com/zking/widget/refreshview/swipeMenu/OnSwipeMenuListener.java
@@ -5,16 +5,31 @@ public interface OnSwipeMenuListener {
 
     /**
      * 点击侧滑删除
-     * @param position          索引位置
+     *
+     * @param position 索引位置
      */
     void toDelete(int position);
 
     /**
      * 点击置顶
-     * @param position          索引位置
+     *
+     * @param position 索引位置
      */
     void toTop(int position);
 
+    /**
+     * 按钮点击事件
+     *
+     * @param obj 对象
+     */
+    void btnClick(Object obj);
 
+    /**
+     * 按钮点击事件
+     *
+     * @param obj   对象
+     * @param index 标识
+     */
+    void btnClick(Object obj, int index);
 
 }
